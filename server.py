8eb9a507a583c23123a4127ae58926a5f1ed49fb
Daniel Elias Bermudez <de.bermudez13@gmail.com>
2018-10-20 11:18:22 -0700

    Create initial server socket code

---
+++ server.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python3
+import socket
+
+HOST = '127.0.0.1'
+PORT = 8080
+
+sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+sock.bind(HOST, PORT)
+
+#TODO need to update 1 to another value
+# 1 represents numbe of connections accepted by the socket
+sock.listen(1)
+conn, address = sock.accept()
+
+conn.send(b'You made a connection. yay!')
+
+sock.close()
+conn.close()
