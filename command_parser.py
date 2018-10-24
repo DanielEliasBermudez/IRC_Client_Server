64bfb63675a7cb09391ef4278404154761872c41
Boris Popadiuk <boris@Boris-MacBook-Pro.local>
2018-10-23 21:53:32 -0700

    add initial joinParser() implementation

--- command_parser.py
+++ command_parser.py
@@ -4,22 +4,27 @@ import argparse
 
 """
 Initial (very early) proof of concept for command parser. Not sure if this is the best route.
-argparse is really useful but i don't think we'll really want to be parsing sys.argv (presumably
-the client will already be running when the command is issued). Haven't dug deep enough into argparse
-yet to know whether it can be leveraged in other ways. Still, this could be a good general framework 
-for parsing the IRC commands. Unfortunately there is enough format variance between commands that some 
-kind of per-command parsing will probably be necessary... this general pattern might be a good way to do it.
+argparse is really useful but has a few issues for our use case, the worst of which is how it handles parsing failures.
+As far as I can tell all it does it print the error the stderr and exit. We can wrap it in a try/except to prevent
+the process from exiting but i'm not sure if it's possible to capture the error type/message. 
+I'll have to look into that further...
+
+Good news is argparse can be fed a list in place of sys.argv, so we could use it to parse commands inside a running
+client or server process.
 """
 
-def parseJoin(args):
+def parseJoin(joinParser):
+    joinParser.add_argument('command', nargs=1, type=str, help='the join command')
+    joinParser.add_argument('channels', nargs=1, type=str, help='comma-separated list of channels to join')
+    joinParser.add_argument('keys', nargs='?', help='optional keys to channels in "channels"')
+    args = joinParser.parse_args()
     print('you parsed JOIN! here are its args: {}'.format(args))
-    # ... and then we parse the args for JOIN
 
-def parseList(args):
+def parseList(listParser):
     print('you parsed LIST! here are its args: {}'.format(args))
     # ... and then we parse the args for LIST
 
-def parsePart(args):
+def parsePart(partParser):
     print('you parsed PART! here are its args: {}'.format(args))
     # ... and then we parse the args for PART
 
@@ -37,6 +42,8 @@ args = parser.parse_args()
 command = ircCommands.get(args.command)
 
 if command:
-    ircCommands[args.command](args.arguments)
+    subparsers = parser.add_subparsers()
+    commandParser = subparsers.add_parser(args.command)
+    ircCommands[args.command](commandParser)
 else:
     parser.print_help()
