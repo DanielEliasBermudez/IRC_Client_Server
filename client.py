ed02965bdd3e3a8817320a2dc930c33b62ebe6e6
Daniel Elias Bermudez <de.bermudez13@gmail.com>
2018-10-25 16:12:46 -0700

    Add json parsing and loading to server.py

--- client.py
+++ client.py
@@ -2,11 +2,12 @@
 
 import socket
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 8080
 
 sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
+sock.send(b"Test Successful")
 message = sock.recv(4096)
-print(message.decode('utf-8'))
+print(message.decode("utf-8"))
 sock.close()
--- server.py
+++ server.py
@@ -1,17 +1,31 @@
 #!usr/bin/env python3
 import socket
+import json
 
+# test json object
+test_python_dict = {
+    "command": "list",
+    "user": "Alice",
+    "target": "room1",
+    "response": "",
+}
+test_json_obj = json.dumps(test_python_dict)
+
+# This will create listening connection TCP socket on localhost:8080
 HOST = "127.0.0.1"
 PORT = 8080
-
-sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-sock.bind((HOST, PORT))
-
-# TODO need to update 1 to another value
-# 1 represents numbe of connections accepted by the socket
-sock.listen(1)
-conn, address = sock.accept()
-
-conn.send(b"You made a connection. yay!")
-
-sock.close()
+# sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT))
+    sock.listen()
+    conn, address = sock.accept()
+    with conn:
+        print("Connected to {}".format(address))
+        data = conn.recv(1024)
+        print(data.decode("utf-8"))
+        # parse the json here
+        msg_json = json.loads(test_json_obj)
+        print(msg_json)
+        conn.send(b"You made a connection. yay!")
+# conn.send(b"You made a connection. yay!")
+# sock.close()
