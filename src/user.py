57566a9c15d1ef63abbf4b079cfadf71f79864c4
Daniel Elias Bermudez <de.bermudez13@gmail.com>
2018-10-25 23:38:26 -0700

    Move code into src directory

---
+++ src/user.py
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self, nick, real_name):
+        self.nick = nick
+        self.real_name = real_name
+
+    def get_nick(self):
+        return self.nick
+
+    def get_real_name(self):
+        return self.real_name
+
+    def __str__(self):
+        return self.real_name
---
+++ test/server_test.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+import unittest
+
+
+class TestServer(unittest.TestCase):
+    # def setUp(self):
+    #     pass
+
+    def test_true(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
