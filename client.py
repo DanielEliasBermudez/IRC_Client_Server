9297a4eea35b3c67fa70c00ffc1a3f926ec8000e
Boris Popadiuk <boris@Boris-MacBook-Pro.local>
2018-10-25 20:49:06 -0700

    get rid of if __name__ ... not needed for now

--- client.py
+++ client.py
@@ -8,18 +8,18 @@ import json
 HOST = "127.0.0.1"
 PORT = 8080
 
-if __name__ == "__main__":
-    args = parser.parseCommand(sys.argv)
-    if args:
-        # convert argparse Namespace object to json string and send to server as byte array
-        json = json.dumps(vars(args)) 
-        sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-        try:
-            sock.connect((HOST, PORT))
-        except ConnectionRefusedError:
-            print('Error: could not connect to server')
-            exit()
-        sock.send(json.encode('utf-8'))
-        message = sock.recv(4096)
-        print(message.decode("utf-8"))
-        sock.close()
+# parse commandline
+args = parser.parseCommand(sys.argv)
+if args:
+    # convert argparse Namespace object to json string and send to server as byte array
+    json = json.dumps(vars(args)) 
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    try:
+        sock.connect((HOST, PORT))
+    except ConnectionRefusedError:
+        print('Error: could not connect to server')
+        exit()
+    sock.send(json.encode('utf-8'))
+    message = sock.recv(4096)
+    print(message.decode("utf-8"))
+    sock.close()
