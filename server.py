7a7877af996fb8e4746d580273747797a13fa3be
Daniel Elias Bermudez <de.bermudez13@gmail.com>
2018-10-25 22:32:07 -0700

    Add function to create rooms

--- server.py
+++ server.py
@@ -1,19 +1,16 @@
 #!usr/bin/env python3
 import socket
 import json
+import user
+import room
 
-HOST = "127.0.0.1"
-PORT = 8080
+test_user_dict = {"command": "user", "nick": "Alice", "real_name": "Alice A"}
+test_user_jobj = json.dumps(test_user_dict)
+test_join_dict = {"command": "join", "room": "test_room", "nick": "Alice"}
+test_join_jobj = json.dumps(test_join_dict)
 
-# test json object
-test_python_dict = {
-    "command": "user",
-    "name": "Alice",
-    "real_name": "Alice A",
-    "server": HOST,
-}
-test_json_obj = json.dumps(test_python_dict)
 list_of_users = []
+list_of_rooms = []
 
 
 def handle_message(msg):
@@ -21,8 +18,17 @@ def handle_message(msg):
     Will look at the message json object and choose the correct handler based on the 
     command key.
     """
-    if msg["command"] == "user":
+    command = msg["command"]
+    if command == "user":
         handle_user_cmd(msg)
+    if command == "join":
+        room = msg["room"]
+        if room in list_of_rooms:
+            handle_join_cmd(msg)
+        else:
+            handle_create_cmd(msg)
+    # if command == "list":
+    #     # handle list call
 
 
 def handle_user_cmd(msg):
@@ -30,35 +36,66 @@ def handle_user_cmd(msg):
     Handles the user command by adding the user to the list of users.
     User json object example:
     {
-        "command" : "user"
-        "name" : "name of user",
+        "command" : "user",
+        "nick" : "name of user",
         "real_name" : "real name of user",
-        "server" : "server ip"
     }
     """
     print("Command - User")
-    print("Name: {}".format(msg["name"]))
+    print("Nick: {}".format(msg["nick"]))
     print("Real Name: {}".format(msg["real_name"]))
-    print("Server: {}".format(msg["server"]))
+    new_user = user.User(msg["nick"], msg["real_name"])
+    list_of_users.append(new_user)
+    print("User added")
+
+
+def handle_create_cmd(msg):
+    """
+    Creates a room when a user tries to join a room that does not exist.
+    User json object example:
+    {
+        "command" : "join",
+        "room" : "name of room"
+        "nick" : "name of user",
+    }
+    """
+    new_room = room.Room(msg["room"])
+    new_room.add_user(msg["nick"])
+    print("Created a room")
+
+
+# TODO update
+def handle_join_cmd(msg):
+    pass
 
 
 # This will create listening connection TCP socket on localhost:8080
-# HOST = "127.0.0.1"
-# PORT = 8080
-# sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT))
-    sock.listen()
-    conn, address = sock.accept()
-    with conn:
-        print("Connected to {}".format(address))
-        data = conn.recv(1024)
-        print(data.decode("utf-8"))
-        # parse the json here - this should be the json that came thru the data. for
-        # now I am just using a test json object
-        msg_json = json.loads(test_json_obj)
-        handle_message(msg_json)
-        conn.send(b"You made a connection. yay!")
+def main():
+    """
+    Run a server on localhost that listens on port 8080
+    
+    """
+    HOST = "127.0.0.1"
+    PORT = 8080
+    with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as sock:
+        sock.bind((HOST, PORT))
+        sock.listen()
+        conn, address = sock.accept()
+        with conn:
+            print("Connected to {}".format(address))
+            data = conn.recv(1024)
+            print(data.decode("utf-8"))
+            # parse the json here - this should be the json that came thru the data. for
+            # now I am just using a test json object
+            msg_json = json.loads(test_user_jobj)
+            handle_message(msg_json)
+            msg_json = json.loads(test_join_jobj)
+            handle_message(msg_json)
+            conn.send(b"You made a connection. yay!")
+
+
+if __name__ == "__main__":
+    main()
 
 # conn.send(b"You made a connection. yay!")
 # sock.close()
