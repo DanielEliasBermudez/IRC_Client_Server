928c11ffde4b192b50ad5fd84a4e0915aac70b20
boris <pboris@pdx.edu>
2018-11-23 23:42:58 -0800

    fix bug that omitted users in names command

--- server.py
+++ server.py
@@ -287,6 +287,8 @@ def handle_quit_cmd(msg, sock):
 def handle_names_cmd(msg):
     command = msg["command"]
     rooms = msg["rooms"]
+    users = set([user.get_nick() for user in list_of_users])
+    print("users: ", users)
     nick_name = msg["nick"]
     room_dict = {}
     reply = ""
@@ -307,7 +309,15 @@ def handle_names_cmd(msg):
         reply += "\n{}: ".format(room)
         for name in room_dict[room]:
             reply += "{}, ".format(name)
+            print("removing name from users list: {}".format(name))
+            users.remove(name)
         reply = reply.rstrip(", ")
+    
+    if len(users) > 0:
+        reply += "\nUsers not in any room: "
+    for user in users:
+        reply += "{}, ".format(user)
+    reply = reply.rstrip(", ")
 
     return build_json_response(command, nick_name, reply)
 
